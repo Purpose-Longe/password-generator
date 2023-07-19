cbfffb73985e64b6c3f2fb83d47ae3abbd68d893
Purpose Longe <purposelonge@gmail.com>
2023-07-18 20:50:07 -0800

    final commit

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const lCase ="abcdefghijklmnopqrstuvwxyz";
 const number ="0123456789";
 const symbol ="!@#$%^&*_";
 
-const pLength = document.getElementById("p.length");
-const upperCase = document.getElementById("p.uppercase");
-const lowerCase = document.getElementById("p.lowercase");
-const pNumber = document.getElementById("p.number");
-const pSymbol = document.getElementById("p.symbol");
+const pLength = document.getElementById("p-length");
+const upperCase = document.getElementById("p-uppercase");
+const lowerCase = document.getElementById("p-lowercase");
+const pNumber = document.getElementById("p-number");
+const pSymbol = document.getElementById("p-symbol");
 const submit = document.getElementById("submit");
 const password = document.getElementById("password");
 
@@ -31,3 +31,17 @@ function generatePassword(l,initialPassword){
         }
         return pass;
 }
+
+//copy password to clipboard
+
+const copy = document.getElementById("copy");
+
+copy.addEventListener("click", () => {
+    if(password.value == ""){
+    alert("Please generate a password")
+}else {
+    password.select();
+    document.execCommand("copy");
+    alert("Password has been copied to clipboard");
+}
+})
